46b00d1a979adf9cfc4cbec760f1a87026bf071a
sterlingwebsite <ssteele1@byupathway.edu>
2026-01-14 10:44:09 -0700

    Modular JavaScript with ES Modules

---
+++ Week 3/scripts/sections.mjs
@@ -0,0 +1,10 @@
+export function populateSections(sections) {
+  const sectionSelect = document.querySelector("#sectionNumber");
+
+  sections.forEach((section) => {
+    const option = document.createElement("option");
+    option.value = section.sectionNumber;
+    option.textContent = `${section.sectionNumber}`;
+    sectionSelect.appendChild(option);
+  });
+}
